0b00bc5fdb2a1b93f498ca763ffa6b0b895cfbd2
TRAVOR SSETTUBA <p1118231@tees.ac.uk>
2024-12-31 21:21:53 +0000

    add connection string

--- imsWeb/obj/Debug/net9.0/imsWeb.AssemblyInfo.cs
+++ imsWeb/obj/Debug/net9.0/imsWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imsWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851650f5232bd7119fff7337abb8ff5029f17c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e70c82dca79b1be546daef8785e48d429beae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("imsWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imsWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
